487f983680fb96dc7158e2c78b5d46262e548e5f
sreekanthpaatil <sreekanthpaatil@gmail.com>
2024-08-03 06:44:21 +0000

    basic db index file setup

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+import path from "path";
+import morgan from "morgan";
+
+import dotenv from "dotenv";
+import connectDB from "./config/mongoDBConfig.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
